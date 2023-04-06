2f702b2723f21e707f2d7e48bf8e1a8a41b95d64
Hubert Barc <barchubert@gmail.com>
2023-04-06 20:18:51 +0200

    things

--- train_resnet.py
+++ train_resnet.py
@@ -8,6 +8,8 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+from torchsummary import summary
+from datetime import date
 from sklearn.neighbors import BallTree
 from torchvision import models
 from torchvision import transforms
@@ -20,14 +22,14 @@ from haversine import haversine
 classes = get_classes()
 
 class ImageDataset(Dataset):
-    def __init__(self, split="train", val_split=0.2, seed=42):
+    def __init__(self, split="train", val_split=0.15, seed=42):
         self.split = split
         self.val_split = val_split
         self.seed = seed
         self.images, self.targets = self.load_data()
 
     def load_data(self):
-        image_files = glob.glob("./download_panoramas/downloads/*.jpg")[:10]
+        image_files = glob.glob("./download_panoramas/downloads/*.jpg")[:100]
         coords = [tuple(map(float, re.findall(r"[-+]?\d+\.\d+", img_path))) for img_path in image_files]
 
         # Convert class coordinates to radians and create a BallTree
@@ -52,26 +54,9 @@ class ImageDataset(Dataset):
         else:  # validation
             combined = combined[:split_index]
         image_files, targets = zip(*combined)
-        print("Data loaded")
+        print(f"{self.split} Data loaded")
 
         return image_files, targets
-    #def load_data(self):
-    #    image_files = glob.glob("./download_panoramas/downloads/*.jpg")[:1000]
-    #    coords = [tuple(map(float, re.findall(r"[-+]?\d+\.\d+", img_path))) for img_path in image_files]
-    #    targets = [np.argmin([haversine(coord, class_coord[1][::-1]) for class_coord in classes]) for coord in coords]
-
-    #    # Shuffle and split the data
-    #    random.seed(self.seed)
-    #    combined = list(zip(image_files, targets))
-    #    random.shuffle(combined)
-    #    split_index = int(len(combined) * self.val_split)
-    #    if self.split == "train":
-    #        combined = combined[split_index:]
-    #    else:  # validation
-    #        combined = combined[:split_index]
-    #    image_files, targets = zip(*combined)
-
-    #    return image_files, targets
 
     def __len__(self):
         return len(self.images)
@@ -83,8 +68,16 @@ class ImageDataset(Dataset):
         target = self.targets[index]
         return image, target
 
+def calculate_geoguessr(outputs, targets, classes_):
+    _, pred_indices = torch.max(outputs, 1)
 
-def custom_loss(outputs, targets, classes_, alpha=0.1):
+    pred_coords = [list(classes_[pred.item()][1]) for pred in pred_indices]
+    target_coords = [list(classes_[t.item()][1]) for t in targets]
+
+    haversine_loss = torch.tensor([haversine(target_coords[i][::-1], pred_coords[i][::-1]) for i in range(targets.size(0))], dtype=torch.float32)
+    return [int(5000*(math.e**(-x/2000))) for x in haversine_loss.tolist()]
+
+def custom_loss(outputs, targets, classes_, alpha=0.001):
     class_loss = nn.CrossEntropyLoss()(outputs, targets)
 
     _, pred_indices = torch.max(outputs, 1)
@@ -92,31 +85,45 @@ def custom_loss(outputs, targets, classes_, alpha=0.1):
     target_coords = [list(classes_[t.item()][1]) for t in targets]
 
     haversine_loss = torch.tensor([haversine(target_coords[i][::-1], pred_coords[i][::-1]) for i in range(targets.size(0))], dtype=torch.float32)
-    haversine_loss = haversine_loss.mean()
 
-    return alpha * haversine_loss + (1 - alpha) * class_loss
+    haversine_loss = torch.mean(haversine_loss)
+
+    haversine_part = alpha * haversine_loss
+    cross_entropy_part = (1 - alpha) * class_loss
+    print(f"Haversine loss part: {haversine_part}")
+    print(f"CrossEntropy loss part: {cross_entropy_part}")
+
+    loss = haversine_part + cross_entropy_part
+    return loss
 
 
-def train(model, dataloader, optimizer, classes_, device):
+def train(model, dataloader, optimizer, classes_, scaler, device):
     model.train()
     total_loss = 0
 
-    for images, targets in dataloader:
+    for idx, (images, targets) in enumerate(dataloader):
         images = images.to(device)
         targets = targets.to(device)
 
         optimizer.zero_grad()
-        outputs = model(images)
-        loss = custom_loss(outputs, targets, classes_)
+
+        with torch.cuda.amp.autocast():
+            outputs = model(images)
+            loss = custom_loss(outputs, targets, classes_)
+
         wandb.log({"step_loss": loss})
-        loss.backward()
-        optimizer.step()
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        if idx % 1000 == 0:
+            print(f"Geoguessr scores on train batch: {calculate_geoguessr(outputs, targets, classes_)}")
 
         total_loss += loss.item()
 
     return total_loss / len(dataloader)
 
-def validate(model, dataloader, classes, device):
+def validate(model, dataloader, classes_, device):
     model.eval()
     total_loss = 0
     num_samples = 0
@@ -127,8 +134,9 @@ def validate(model, dataloader, classes, device):
             targets = targets.to(device)
 
             outputs = model(images)
-            loss = custom_loss(outputs, targets, classes)
+            loss = custom_loss(outputs, targets, classes_)
 
+            print(f"Geoguessr scores on validate batch: {calculate_geoguessr(outputs, targets, classes_)}")
             total_loss += loss.item() * targets.size(0)
             num_samples += targets.size(0)
 
@@ -139,8 +147,8 @@ def main():
     wandb.init(project='geoguessr-ai', entity='desik')
 
     config = wandb.config
-    config.learning_rate = 0.01
-    config.batch_size = 10
+    config.learning_rate = 0.1
+    config.batch_size = 16
     config.epochs = 1000
 
     train_dataset = ImageDataset(split="train")
@@ -152,36 +160,41 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     num_classes = len(classes)
 
-    model = models.resnet50(pretrained=True)
-    for param in model.parameters():
+    model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+
+    for param in list(model.parameters())[:84]:
         param.requires_grad = False
 
     # Modify the first layer to accommodate the larger input
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(15, 15), stride=(4, 4), padding=(6, 6), bias=False)
-    model = model.to(device)
 
     # Replace the last layer with a fully connected layer for our number of classes
     model.fc = nn.Linear(model.fc.in_features, num_classes)
+    #summary(model, (3, 1664, 832))
+
     model = model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    scaler = torch.cuda.amp.GradScaler()
 
 
     min_val_loss = math.inf
-    for epoch in range(config.epochs):
-        loss = train(model, train_dataloader, optimizer, classes, device)
 
-        train_loss = train(model, train_dataloader, optimizer, classes, device)
+    for epoch in range(config.epochs):
+        train_loss = train(model, train_dataloader, optimizer, classes, scaler, device)
         val_loss = validate(model, val_dataloader, classes, device)
+
         wandb.log({"train_loss": train_loss})
         wandb.log({"val_loss": val_loss})
+
         print(f"Epoch {epoch + 1}/{config.epochs}, Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
+
         scheduler.step()
         if val_loss < min_val_loss:
             min_val_loss = val_loss
-            torch.save(model.state_dict(), f"geoguessr-{epoch}.pth")
-            wandb.save("geoguessr-{epoch}.pth")
+            torch.save(model.state_dict(), f"{date.today()}-geoguessr-{epoch}.pth")
+            wandb.save(f"{date.today()}-geoguessr-{epoch}.pth")
 
 
 if __name__ == '__main__':
